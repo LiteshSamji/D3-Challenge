7a89d6050ba0cf7efd5208102f631977460e69ef
Litesh Samji <litesh@y7mail.com>
2021-08-26 20:24:08 +1000

    chart dimension

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,26 @@
 // @TODO: YOUR CODE HERE!
+//Chart dimensions
+var svgWidth = 960;
+var svgHeight = 620;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+var width = svgWidth - margin.right - margin.left;
+var height = svgHeight - margin.top - margin.bot
+
+//Append DIV class to ccatter element
+var chart = d3.select("#scatter").append("div").classed("chart", true);
+
+// Append an SVG group
+var svg = chart.append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+    //initial Parameters
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
