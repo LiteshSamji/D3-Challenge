64018d7ea4dcc6c536abb1b8556835a9611ba856
Litesh Samji <litesh@y7mail.com>
2021-08-28 12:57:29 +1000

    Variables to hold X and Y axis data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,4 @@
-// @TODO: YOUR CODE HERE!
-//Chart dimensions
+//Define chart area
 var svgWidth = 960;
 var svgHeight = 620;
 
@@ -27,5 +26,42 @@ var chosenYAxis = "healthcare";
 
 //retrieve csv data and execute everything below
 d3.csv("./assets/data/data.csv").then(function(censusData) {
-
     console.log(censusData);
+
+    // parse data
+    censusData.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+
+    //create first linear scales
+    var xLinearScale = xScale(censusData, chosenXAxis);
+    var yLinearScale = yScale(censusData, chosenYAxis);
+
+    //create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    
+    //append x axis
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    //append y axis
+    var yAxis = chartGroup.append("g")
+        .classed("y-axis", true)
+        .call(leftAxis);
+
+    //append initial text
+    var textGroup = chartGroup.selectAll(".stateText")
+        .data(censusData)
+        .enter()
+        .append("text")
+        .classed("stateText", true)
+        .attr("x", d => xLinearScale(d[chosenXAxis]))
+        .attr("y", d => yLinearScale(d[chosenYAxis]))
+        .attr("dy", 3)
+        .attr("font-size", "10px")
+        .text(function(d){return d.abbr});
+
+});
